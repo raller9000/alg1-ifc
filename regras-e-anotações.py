dc4353321583534fc7aa4c4c957e2473c6dcc566
raller9000 <gustavorgb4382@gmail.com>
2026-03-03 18:52:36 -0300

    Update on .py file

---
+++ regras-e-anotações.py
@@ -0,0 +1,8 @@
+# Este arquivo é destinado a informações chave e lembretes relacionados as aulas.
+# 
+# Submissão de listas de exercícios:
+# 
+# Exercícios devem ser entregues no Google Classroom. Para cada um dos exercícios,
+# Crie um arquivo fonte Python com o respectivo nome de acordo com a seguinte regra: SUASINICIAIS-Alg-01-Ex-num.py. 
+# Por exemplo, se o professor resolvesse o exercício número 3, o nome do arquivo seria PCRG-Alg-01-Ex-03.py.
+#
