71197755e50608a58615fd2caaa4559690e80edd
rrribeiro <gustavorgb4382@gmail.com>
2026-03-13 12:24:34 -0300

    12 feito

---
+++ GRGB-Alg-01-Ex-12.py
@@ -0,0 +1,9 @@
+import math
+
+r = float(input("Digite o raio \n"))
+
+# erroooooo no exerciciooooo falto o 4 * na area
+volume = (4/3) * math.pi * (r ** 3)
+area = 4 * math.pi * (r ** 2)
+
+print(f"volume da esfera: {volume:.2f} \narea da esfera: {area:.2f} \n")
