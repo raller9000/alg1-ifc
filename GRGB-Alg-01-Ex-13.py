7ed9700d7109d546558a4b81d8d88ba34a05b410
rrribeiro <gustavorgb4382@gmail.com>
2026-03-13 12:26:35 -0300

    13 feito

---
+++ GRGB-Alg-01-Ex-13.py
@@ -0,0 +1,6 @@
+b = float(input("Digite o comprimento da base em cm. \n"))
+h = float(input("Digite a altura em cm. \n"))
+
+area = (b * h) / 2
+
+print("area do triangulo ", f"{area:.2f}", "cm^2 \n")
