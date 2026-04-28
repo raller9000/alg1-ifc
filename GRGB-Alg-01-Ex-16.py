de51db99f7adbecd8f54504e0d5c67973ce5239e
rrribeiro <gustavorgb4382@gmail.com>
2026-04-28 20:41:41 -0300

    Lista completa

--- GRGB-Alg-01-Ex-16.py
+++ GRGB-Alg-01-Ex-16.py
@@ -1,1 +1,38 @@
-#
+#heads or tails!!!
+
+import random
+
+i = 0
+ac = ""
+ac2 = ""
+fullac = ""
+total = 0
+fulltotal = 0
+
+while i < 10:
+
+    if ac == "AAA" or ac2 == "OOO":
+        print(fullac)
+        ac2 = ""
+        ac = ""
+        i += 1
+        total = fullac.count("A") + fullac.count("O")
+        print(f"total de sorteios: {total}")
+        fulltotal = fulltotal + total
+        fullac = ""
+
+    else:
+        x = random.randint(0,1)
+
+        if x == 0:
+            ac = ac + "A"
+            ac2 = ""
+            fullac = fullac + "A"
+
+        else:
+            ac2 = ac2 + "O"
+            ac = ""
+            fullac = fullac + "O"
+
+med = fulltotal/10
+print(f"media de sorteios: {med:.2f}")
